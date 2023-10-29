145c212dcedd790395ff3d33d348e1ea922afc09
Alireza Hoseini <alireza.hoseini20031382@gmail.com>
2023-10-29 18:38:20 +0330

    Writing Unit test

---
+++ app/src/androidTest/java/com/example/cso/UITest.java
@@ -0,0 +1,25 @@
+package com.example.cso;
+
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.checkerframework.checker.units.qual.A;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(TestRunner.class)
+public class UITest {
+
+    @Test
+    public void restoreButtonExists(){
+//
+//        Espresso.onView(ViewMatchers.withId(R.id.))
+//                .check(matches(withText("Restore")));
+    }
+}
