dc2606d6ee3cee72d9c6ff01d57821b11ae7e350
Alireza Hoseini <alireza.hoseini20031382@gmail.com>
2023-10-30 21:31:04 +0330

    Adding UI unit tests

--- app/src/androidTest/java/com/example/cso/UITest.java
+++ app/src/androidTest/java/com/example/cso/UITest.java
@@ -3,10 +3,9 @@ package com.example.cso;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import com.example.cso.R;
-
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
 
 import org.junit.Before;
@@ -18,13 +17,30 @@ public class UITest {
 
     @Before
     public void setUp() {
-        ActivityScenario.launch(MainActivity.class); // Replace YourActivity with your actual activity class
+        ActivityScenario.launch(MainActivity.class);
     }
 
     @Test
     public void restoreButtonExists(){
-
         Espresso.onView(ViewMatchers.withId(R.id.restoreButton))
                 .check(matches(withText("Restore")));
+    }
+
+    @Test
+    public void headerExists(){
+        Espresso.onView(ViewMatchers.withId(R.id.headerTextView)).
+                check(ViewAssertions.matches(ViewMatchers.withText("CSO")));
+
+        Espresso.onView(ViewMatchers.withId(R.id.infoButton))
+                .check(matches(withText("i")));
+    }
+
+    @Test
+    public void drawerLayoutExists(){
+        Espresso.onView(ViewMatchers.withId(R.id.drawer_layout))
+                .check(ViewAssertions.doesNotExist());
+
+        Espresso.onView(ViewMatchers.withId(R.id.navigationView))
+                .check(ViewAssertions.doesNotExist());
     }
 }
