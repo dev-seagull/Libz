3b24770ab3dde490f8869bc36844d74ccfb3102a
Alireza Hoseini <alireza.hoseini20031382@gmail.com>
2024-03-14 16:17:48 +0330

    Fixed two apks build bug

--- app/src/main/java/com/example/cso/SplashActivity.java
+++ app/src/main/java/com/example/cso/SplashActivity.java
@@ -8,24 +8,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 3000; // 3 seconds
+    private static final int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
 
-                // close this activity
-                finish();
-            }
+            finish();
         }, SPLASH_TIME_OUT);
     }
 }
